bdd0cc2dd4a8f9599b5d85c8786cda8adb2537d5
황인태 <ghkddlsxo55@gmail.com>
2022-03-28 15:39:13 +0900

    0328모작진행

--- WinApi2D/CCreateState.cpp
+++ WinApi2D/CCreateState.cpp
@@ -1,5 +1,6 @@
 #include "framework.h"
 #include "CCreateState.h"
+#include "CGameObject.h"
 #include "CMonster.h"
 #include "CAcorn.h"
 #include "CAnimator.h"
@@ -15,13 +16,21 @@ CCreateState::~CCreateState()
 
 void CCreateState::CreateAcorn()
 {
+	//TODO : 여기 생성이 안되니까 몬스터 클래스에서 생성을 시도
 	fPoint acornPos = pMonster->GetPos();
 
 	CAcorn* acorn = new CAcorn;
+	acorn->SetPos(acornPos);
+	if (crafting == true)
+	{
+		acorn->SetDir(fVec2(-1, 0));
+		acornPos.x += pMonster->GetScale().x / 2.f;
+	}
 }
 
 void CCreateState::update()
 {
+	crafting = false;
 	m_fDT += fDT;
 	pMonster = GetMonster();
 	//Creat 애니메이션 출력하고
@@ -30,11 +39,12 @@ void CCreateState::update()
 	{
 		pMonster->GetAnimator()->Play(L"MonCreateIng");
 		if (m_fDT >= 5.f) {
+			crafting = true;
+			CreateAcorn();
 			ChangeAIState(GetOwnerAI(), STATE_MON::IDLE);
 		}
 	}
 	// 애니메이션 중간에 도토리 3개를 손안에 3개 출력하고
-	
 	// 플레이어 위치를 알고 거기로 일직선으로 나가게
 	// 애니메이션이 끝나면 Idle상태로 출력
 	
--- WinApi2D/CCreateState.h
+++ WinApi2D/CCreateState.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "CState.h"
+
+class CAcorn;
+
 class CCreateState : public CState
 {
 private:
 	float m_fDT;
 
 	CMonster* pMonster;
+	CAcorn* pAcorn;
+
+	bool crafting = false;
 public:
 	CCreateState(STATE_MON state);
 	virtual ~CCreateState();
